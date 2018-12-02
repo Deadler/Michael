f04f3b37c2ae584c982dd3066a91261593d3655b
Deadler <45537613+Deadler@users.noreply.github.com>
2018-12-02 16:15:33 +0100

    Add files via upload

---
+++ Assets/Gun.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Gun : MonoBehaviour {
+    public GameObject enemy;
+    private float[,] matriks_4x4 = {
+        { 1, 0, 0, 0 },
+        { 0, 1, 0, 0 },
+        { 0, 0, 1, 0 },
+        { 0, 0, 0, 1 }
+    };
+    private float[,] vec_position = { { 0 }, { 0 }, { 0 }, { 1 } };
+    // Use this for initialization
+    void Start()
+    {
+
+        //private float[,] vec_position = { {this.transform.rotation.x}, { this.transform.rotation.y }, { this.transform.rotation.z }, { 1 } };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[,] enemy_pos = { { enemy.transform.rotation.x }, { enemy.transform.rotation.y }, { enemy.transform.rotation.z }, { 1 } };
+        print(enemy_pos[1, 0]);
+        //transformRotX(x_ang);
+        float[,] rot_x = { { 1 }, { 0 }, { 0 }, { 1 } };
+        float[,] rot_y = { { 0 }, { 1 }, { 0 }, { 1 } };
+        float[,] rot_z = { { 0 }, { 0 }, { 1 }, { 1 } };
+
+        /*float x_ang = Mathf.Acos((rot_x[0,0] * enemy_pos[0,0] + rot_x[1, 0] * enemy_pos[1, 0] + rot_x[2, 0] * enemy_pos[2, 0])/
+            Mathf.Sqrt(rot_x[0, 0] * rot_x[0, 0] + rot_x[1, 0] * rot_x[1, 0] + rot_x[2, 0] * rot_x[2, 0]) *
+            Mathf.Sqrt(enemy_pos[0, 0] * enemy_pos[0, 0] + enemy_pos[1, 0] * enemy_pos[1, 0] + enemy_pos[2, 0] * enemy_pos[2, 0]));
+        
+        float y_ang = Mathf.Acos((rot_y[0, 0] * enemy_pos[0, 0] + rot_y[1, 0] * enemy_pos[1, 0] + rot_y[2, 0] * enemy_pos[2, 0]) /
+            Mathf.Sqrt(rot_y[0, 0] * rot_y[0, 0] + rot_y[1, 0] * rot_y[1, 0] + rot_y[2, 0] * rot_y[2, 0]) *
+            Mathf.Sqrt(enemy_pos[0, 0] * enemy_pos[0, 0] + enemy_pos[1, 0] * enemy_pos[1, 0] + enemy_pos[2, 0] * enemy_pos[2, 0])); ;
+
+        float z_ang = Mathf.Acos((rot_z[0, 0] * enemy_pos[0, 0] + rot_z[1, 0] * enemy_pos[1, 0] + rot_z[2, 0] * enemy_pos[2, 0]) /
+            Mathf.Sqrt(rot_z[0, 0] * rot_z[0, 0] + rot_z[1, 0] * rot_z[1, 0] + rot_z[2, 0] * rot_z[2, 0]) *
+            Mathf.Sqrt(enemy_pos[0, 0] * enemy_pos[0, 0] + enemy_pos[1, 0] * enemy_pos[1, 0] + enemy_pos[2, 0] * enemy_pos[2, 0])); ;
+        print(Mathf.Cos(z_ang));*/
+
+        float y = enemy.transform.position.y - transform.position.y;
+        float x = enemy.transform.position.x - transform.position.x;
+        float z = enemy.transform.position.z - transform.position.z;
+
+        float x_ang = enemy.transform.rotation.x;
+        float y_ang = enemy.transform.rotation.y;
+        float z_ang = enemy.transform.rotation.z;
+        float[,] vec_position_y = Multiplication(transformRotY(y_ang), rot_y);
+        float[,] vec_position_x = Multiplication(transformRotX(x_ang), vec_position_y);
+        float[,] vec_position = Multiplication(transformRotZ(z_ang), vec_position_x);
+
+
+
+        
+        //vec_position = Multiplication
+        //vec_position = Multiplication(Multiplication(Multiplication(transformRotZ(z_ang), rot_z), Multiplication(transformRotX(x_ang), rot_x)), Multiplication(transformRotY(y_ang), rot_y));
+        float f = transform.rotation.x;
+        //Vector3 rotationVector;
+        //rotationVector = new Vector3(vec_position[0, 0], vec_position[1, 0], vec_position[2, 0]);
+        //transform.Rotate(rotationVector);
+        float[,] rotatedForward = { { vec_position[0,0]}, { vec_position[0, 1] }, { vec_position[0, 2] }, { 1} };
+        float[,] rotatedUp = { { vec_position[0, 0] }, { vec_position[0, 1] }, { vec_position[0, 2] }, { 1 } };
+        //transform.LookAt(Multiplication());
+    }
+
+    private float[,] transformRotX(float teta)
+    {
+        float[,] matr = matriks_4x4;
+        matr[1, 1] = Mathf.Cos(teta);
+        matr[1, 2] = -Mathf.Sin(teta);
+        matr[2, 1] = Mathf.Sin(teta);
+        matr[2, 2] = Mathf.Cos(teta);
+        return matr;
+    }
+    private float[,] transformRotY(float teta)
+    {
+        float[,] matr = matriks_4x4;
+        matr[0, 0] = Mathf.Cos(teta);
+        matr[0, 2] = Mathf.Sin(teta);
+        matr[2, 0] = -Mathf.Sin(teta);
+        matr[2, 2] = Mathf.Cos(teta);
+        return matr;
+    }
+    private float[,] transformRotZ(float teta)
+    {
+        float[,] matr = matriks_4x4;
+        matr[0, 0] = Mathf.Cos(teta);
+        matr[0, 1] = -Mathf.Sin(teta);
+        matr[1, 0] = Mathf.Sin(teta);
+        matr[1, 1] = Mathf.Cos(teta);
+        return matr;
+    }
+
+    static float[,] Multiplication(float[,] a, float[,] b)
+    {
+        if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+        float[,] r = new float[a.GetLength(0), b.GetLength(1)];
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < b.GetLength(1); j++)
+            {
+                for (int k = 0; k < b.GetLength(0); k++)
+                {
+                    r[i, j] += a[i, k] * b[k, j];
+                }
+            }
+        }
+        print(r[2, 0]);
+        return r;
+    }
+}
---
+++ Assets/Matr.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Matr : MonoBehaviour
+    {
+        public GameObject mirror;
+        private Matrix3x3 mtr;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            mtr = Matrix3x3.Euler(mirror.transform.rotation.x, mirror.transform.rotation.y, mirror.transform.rotation.z);
+            Vector3 vectorFoward = Matrix3x3.multiply(mtr, Vector3.forward);
+            Vector3 vectorUp = Matrix3x3.multiply(mtr, Vector3.up);
+            print(mtr.matr[0,0]);
+            print(mtr.matr[0, 1]);
+            print(mtr.matr[0, 2]);
+            print(mtr.matr[1, 0]);
+            print(mtr.matr[1, 1]);
+            print(mtr.matr[1, 2]);
+            print(mtr.matr[2, 0]);
+            print(mtr.matr[2, 1]);
+            print(mtr.matr[2, 2]);
+            print("");
+            Debug.DrawLine(new Vector3(0, 0, 0), vectorFoward);
+            Debug.DrawLine(new Vector3(0, 0, 0), vectorUp);
+            //transform.rotation.x = Matrix3x3.multiply(mtr, new Vector3(1,0,0));
+            //print(vectorFoward + " " + Vector3.forward);
+            //transform.rotation = 
+            //Matrix3x3 mtr = 
+            //mirror.transform.rotation;
+            transform.LookAt(vectorFoward, vectorUp);
+            //transform.position += Vector3.forward * Time.deltaTime;
+            //transform.position += Vector3.up * Time.deltaTime;
+        }
+    }
+}
